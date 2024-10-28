2ea01a395101d68f25ad81ae7406a1cced360486
Netanel Rotenstein <email@example.com>
2024-10-28 11:28:49 +0200

    unitialize

---
+++ todo/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import Add from './componnents/Add'
+import List from './componnents/List'
+import Todo from './models/TodoModel'
+
+export default function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  return (
+    <div className='app'>
+      <Add />
+      <List />
+    </div>
+  )
+}
+
